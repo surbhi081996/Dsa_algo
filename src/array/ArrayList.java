cd7eea2e8f6c3c83d9dd29c45663096dcf15f9a7
surbhikarajgir <surbhikarajgir@localhost>
2023-05-30 12:25:52 +0530

    ArrayList questions

---
+++ src/array/ArrayList.java
@@ -0,0 +1,19 @@
+package array;
+
+import java.util.List;
+
+public class ArrayList {
+	
+	public static void main(String args[])
+	{
+	 List<String> ar = new java.util.ArrayList<String>();
+	 ar.add("pink");
+	 ar.add("red");
+	 ar.add("yellow");
+	 ar.add("black");
+	 System.out.println(ar);
+		
+		
+	}
+
+}
